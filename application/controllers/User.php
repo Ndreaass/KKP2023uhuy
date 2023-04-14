d680bf647be7f82a7dd41fcea1a466f8267c8f56
Ndreaass <ndreaass@gmail.com>
2023-04-14 23:50:05 +0700

    first commit

---
+++ application/controllers/User.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    public function index()
+    {
+
+        $data['title'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('template/footer');
+    }
+}
